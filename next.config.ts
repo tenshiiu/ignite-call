5c859604ff65cda619b6dd5dfd1038bf09bae44a
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2025-05-30 11:04:16 -0300

    Configuração do projeto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
