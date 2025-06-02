1cfd12bcd27b571c879be6205ced63dcef316d57
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2025-06-02 10:56:44 -0300

    Hook Form, e validação com Zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,10 +1,31 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
+import { useForm } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
+import {zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string()
+  .min(3, { message: 'O usuário precisa ter pelo menos 3 caracteres.' })
+  .regex(/^([a-z\\\\-]+)$/i, { message: 'O usuário pode conter apenas letras e hifens.'})
+  .transform((username) => username.toLowerCase()),
+})
+
+type ClaimsUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm<ClaimsUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema)
+  })
+
+  async function handleClaimUsername(data: ClaimsUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as='form'>
+    <>
+    <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size="sm"
         prefix="ignite.com/"
@@ -12,11 +33,21 @@ export function ClaimUsernameForm() {
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
         crossOrigin={undefined}
+        {...register('username')}
       />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
       </Button>
     </Form>
+
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado.'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
