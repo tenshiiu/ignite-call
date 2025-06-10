f29a112b57b3b647b4b1909d0a8afcc83168f886
Raul Vinícius <raulferreiragg11@gmail.com>
2025-06-10 11:43:56 -0300

    Input e Output do Zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -21,6 +21,7 @@ import {
   IntervalInputs,
   IntervalItem,
 } from './styles'
+import { convertTimeStringToMinutes } from '../../../utils/conver-time-string-to-minutes'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -36,10 +37,30 @@ const timeIntervalsFormSchema = z.object({
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine((intervals) => intervals.length > 0, {
       message: 'Você precisa selecionar pelo menos um dia da semana',
-    }),
+    })
+    .transform(intervals => {
+        return intervals.map(interval => {
+            return {
+                weekDay: interval.weekDay,
+                startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+                endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+            }
+        })
+    })
+    .refine((intervals) => {
+        return intervals.every(
+            (interval) => 
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes
+        )
+    }, {
+        message: 'O horário de término deve ser pelo menos 1h distante do início.',
+    })
+    ,
 })
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -72,7 +93,7 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
+  async function handleSetTimeIntervals(data: TimeIntervalsFormOutput) {
     console.log(data)
   }
 
