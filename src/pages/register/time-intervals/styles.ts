1eda6f81e25a36fac791eb6b204507c64595acfd
Raul Vinícius <raulferreiragg11@gmail.com>
2025-06-10 10:33:00 -0300

    Validação e transformação no Zod

--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react";
+import { Box, styled, Text } from "@ignite-ui/react";
 
 export const IntervalBox = styled(Box, {
     marginTop: '$6',
@@ -6,7 +6,7 @@ export const IntervalBox = styled(Box, {
     flexDirection: 'column',
 })
 
-export const IntervalsContainer = styled('div', {
+export const IntervalContainer = styled('div', {
     border: '1px solid $gray600',
     borderRadius: '$md',
     marginBottom: '$4',
@@ -39,3 +39,7 @@ export const IntervalInputs = styled('div', {
   },
 })
 
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
+})
